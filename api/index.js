7841f2db191c09f475ebc7397d0be994653911ff
Vinod-Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-09-07 19:20:19 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"; // install dotenv in the backend because we cannot have .env file in the backend
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("\x1b[32m%s\x1b[0m", "----- Connected to MongoDB! ----- ");
+  })
+  .catch((err) => {
+    console.log('\x1b[31m%s\x1b[0m',err);
+  });
 
 const app = express();
 
-app.listen(3000, () =>{
-  console.log('Server is live on port 3000!');
-  
-})
+app.listen(3000, () => {
+  console.log("\x1b[36m%s\x1b[0m", "----- Server is live on port 3000! ----- ");
+});
