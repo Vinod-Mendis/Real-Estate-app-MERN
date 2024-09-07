c9737c11c874f008b2a8fe47b2683f05ecc75635
Vinod-Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-09-07 20:06:01 +0700

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"; // install dotenv in the backend because we cannot have .env file in the backend
+import useRouter from "./routes/user.route.js";
 
 dotenv.config();
 mongoose
@@ -9,7 +10,7 @@ mongoose
     console.log("\x1b[32m%s\x1b[0m", "----- Connected to MongoDB! ----- ");
   })
   .catch((err) => {
-    console.log('\x1b[31m%s\x1b[0m',err);
+    console.log("\x1b[31m%s\x1b[0m", err);
   });
 
 const app = express();
@@ -17,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("\x1b[36m%s\x1b[0m", "----- Server is live on port 3000! ----- ");
 });
+
+app.use("/api/user", useRouter);
