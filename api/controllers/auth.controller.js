ef23b19a9e233e1d7f078d33e9adaea972d764dc
Vinod-Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-09-08 15:17:06 +0700

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jsonwebtoken from "jsonwebtoken";
 
 /* Sign up function
 -------------------------------------------- */
@@ -10,6 +12,33 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(200).json("User Created Successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* Sign up function
+-------------------------------------------- */
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(401, "Invalid Credentials!"));
+    }
+    const token = jsonwebtoken.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
