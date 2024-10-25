897a624cb4b8d9dc550fead79817c1406e139643
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-10-25 15:50:33 +0700

    complete show listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
@@ -27,9 +29,11 @@ export default function Profile() {
   const [image, setImage] = useState(undefined);
   const [uploadProgress, setuploadProgress] = useState(0);
   const [fileuploadError, setfileuploadError] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
   const { currentUser, loading, error } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   useEffect(() => {
     if (image) {
@@ -123,6 +127,21 @@ export default function Profile() {
     }
   };
 
+  const handleShowListing = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listing/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-12">Profile</h1>
@@ -184,14 +203,12 @@ export default function Profile() {
         />
         <button
           disabled={loading}
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition disabled:opacity-75"
-        >
+          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition disabled:opacity-75">
           {loading ? "Loading..." : "Update"}
         </button>
         <Link
           className="bg-green-700 text-white p-3 rounded-lg hover:bg-green-800 text-center transition disabled:opacity-75"
-          to={"/create-listing"}
-        >
+          to={"/create-listing"}>
           Create Listing
         </Link>
       </form>
@@ -208,6 +225,42 @@ export default function Profile() {
       <p className="text-green-600 mt-4 ">
         {updateSuccess ? "User Update Successfully!" : ""}
       </p>
+      <button
+        onClick={handleShowListing}
+        className="bg-gray-300 w-full text-gray-800 p-3 rounded-lg hover:bg-gray-400 transition disabled:opacity-75">
+        Show Listings
+      </button>
+      <p className="text-red-500 mt-4 ">
+        {showListingsError ? "Error Showing Listings!" : ""}
+      </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4 mt-10">
+          <h1 className="font-semibold text-xl text-gray-500">Your Listings</h1>
+          {userListings.map((listing) => (
+          <div
+            key={listing._id}
+            className="flex justify-between border border-slate-50 hover:bg-slate-50 p-4 rounded-xl hover:shadow-lg transition">
+            <Link
+              to={`/listing/${listing._id}`}
+              className="flex justify-between w-full items-center">
+              <img
+                src={listing.imageUrls[0]}
+                alt="listing image"
+                className="h-full w-16 object-cover rounded-lg"
+              />
+              <p className="font-semibold flex-1 pl-4 truncate">
+                {listing.name}
+              </p>
+            </Link>
+            <div>
+              <button className="text-blue-500 font-medium">Edit</button>
+              <button className="text-red-400 font-medium">Delete</button>
+            </div>
+          </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
