3b98c21df3c97ae5fca25323a590ad3cf2b63d3b
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2024-10-29 21:20:12 +0700

    fix bug in fetching lisitings api endpoint

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,34 +64,37 @@ export const fetchListingData = async (req, res, next) => {
 
 export const fetchListings = async (req, res, next) => {
   try {
-    const limit = parent(req.query.limit) || 9;
+    const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
 
-    if (offer === undefined || offer === false) {
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
-    if (furnished === undefined || furnished === false) {
+
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
-    if (parking === undefined || parking === false) {
+
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
+
     if (type === undefined || type === "all") {
-      type = { $in: ["rent", "sale"] };
+      type = { $in: ["sale", "rent"] };
     }
 
     const searchTerm = req.query.searchTerm || "";
 
-    const sort = req.params.sort || "createdAt";
+    const sort = req.query.sort || "createdAt";
 
-    const order = req.params.order || "desc";
+    const order = req.query.order || "desc";
 
     const listings = await Listing.find({
       name: { $regex: searchTerm, $options: "i" },
